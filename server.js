c4d47e600d4aa7ea1d62c645cde7d568ed073e45
Bill Hefty <billhefty@gmail.com>
2016-11-05 19:24:39 +0000

    finish API functionality

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 'use strict'
 
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.get('/:date', function(req, res) {
+    let timestamp = req.params.date
+    
+    let valid = ((new Date(timestamp)).getTime() > 0) || +timestamp > 0
+    
+    if (valid) {
+        let unix, natural
+        if (isNaN(+timestamp)) {
+            unix = Date.parse(timestamp) / 1000
+            natural = timestamp
+        } else {
+            unix = timestamp
+            natural = ((new Date(timestamp * 1000)).toDateString()).substring(4)
+        }
+        
+        let result = {
+            unix: unix,
+            natural: natural
+        }
+        res.json(result)
+    } else {
+        res.json({ unix: null, natural: null })
+    }
+    
+})
+
 app.get('/', function(req, res) {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(8080, function() {
